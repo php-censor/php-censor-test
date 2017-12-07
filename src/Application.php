23e7c8d28d48ca2a235e15e4cee4afc3d9427f48
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-12-07 20:00:42 +0700

    TODO test

--- src/Application.php
+++ src/Application.php
@@ -6,12 +6,14 @@ class Application
 {
     public function success()
     {
+        //TODO: 1
         echo 'success command';
         exit(0);
     }
 
     public function failed()
     {
+        //TODO: 2
         echo 'failed command';
         exit(1);
     }
