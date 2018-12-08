7230272c5d4e1966c6286c02cb04f2fdf78b604d
Dmitry Khomutov <poisoncorpsee@gmail.com>
2018-12-08 12:56:57 +0700

    Testing

--- tests/src/ApplicationTest.php
+++ tests/src/ApplicationTest.php
@@ -9,6 +9,90 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
     public function testSample()
     {
         $application = new Application();
+        
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample2()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample3()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample4()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample5()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample6()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample7()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample8()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample9()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
+        $this->assertInstanceOf('PHPCensorTest\\Application', $application);
+    }
+
+    public function testSample10()
+    {
+        $application = new Application();
+
+        sleep(10);
+        
         $this->assertInstanceOf('PHPCensorTest\\Application', $application);
     }
 }
